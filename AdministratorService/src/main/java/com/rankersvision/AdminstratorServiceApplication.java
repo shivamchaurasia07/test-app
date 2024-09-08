a2e3684ca00621b56fef971a4a1e78833d8e2050
helloshivam142 <helloshivam142@gmail.com>
2024-09-08 22:43:12 +0530

    new changes

---
+++ AdministratorService/src/main/java/com/rankersvision/AdminstratorServiceApplication.java
@@ -0,0 +1,14 @@
+package com.rankersvision;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminstratorServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminstratorServiceApplication.class, args);
+    }
+}
